f358cc9b3c9ab93d4eea62778fd74ca8bbfb4ca9
parkinhwa <>
2021-09-28 10:43:08 +0000

    ..

--- BaekJoon_code/Level1/별_찍기2.js
+++ BaekJoon_code/Level1/별_찍기2.js
@@ -2,5 +2,5 @@ let input = require('fs').readFileSync('/dev/stdin').toString();
 let star = Array.from({length:input}, ()=>" ")
 for(let i = Number(input)-1; i > -1; i--){
   star[i] = "*"
-  console.log(star.join(""))
+  console.log(star.join(''))
 }

0c2a84bdb5f72ec48731fee251799f105e4b89e0
parkinhwa <inhwa6548@naver.com>
2021-11-04 07:18:36 +0000

    ..

--- BaekJoon_code/Practice/Test/01.js
+++ BaekJoon_code/Practice/Test/01.js
@@ -1,22 +1,28 @@
-function solution(v) {
-    var answer = [];
-
-    for(let i = 0; i < v.length; i++){
-        if(answer.includes(v[i][0])){
-            answer.splice(answer.indexOf(v[i][0]), 1);
-        } else {
-            answer.push(v[i][0])
-        }          
-    }
-   // console.log(answer) 
-    for(let j = 0; j < v.length; j++){
-        // console.log(answer)
-        if(answer[answer.length-1] == v[j][1]){
-            answer.pop();
-        } else {
-            answer.push(v[j][1])
-        } 
-    }
-    // console.log(answer)
-    return answer;
+function solution(v){
+  let answer = [];
+  answer[0] = v[0][0]^v[1][0]^v[2][0];
+  answer[1] = v[0][1]^v[1][1]^v[2][1];
+  return answer;
 }
+// function solution(v) {
+//     var answer = [];
+
+//     for(let i = 0; i < v.length; i++){
+//         if(answer.includes(v[i][0])){
+//             answer.splice(answer.indexOf(v[i][0]), 1);
+//         } else {
+//             answer.push(v[i][0])
+//         }          
+//     }
+//    // console.log(answer) 
+//     for(let j = 0; j < v.length; j++){
+//         // console.log(answer)
+//         if(answer[answer.length-1] == v[j][1]){
+//             answer.pop();
+//         } else {
+//             answer.push(v[j][1])
+//         } 
+//     }
+//     // console.log(answer)
+//     return answer;
+// }

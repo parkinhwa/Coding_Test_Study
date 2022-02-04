49d8687591bd4a5ed8db3b191ef4b0ce0bf756bc
parkinhwa <inhwa6548@naver.com>
2022-02-04 04:28:36 +0000

    ..

--- Programmers_code/Corporation_CT/신고결과받기.js
+++ Programmers_code/Corporation_CT/신고결과받기.js
@@ -11,17 +11,31 @@ function solution(id_list, report, k) {
     for(let i = 0; i < report.length; i++){
       let content = report[i].split(" ");
       rep.push([content[0], content[1]]);
-      dec.push(content[1]);
     }
     // 2차원 배열 key object로 변환
-    const arrayToObject = (array) =>
-    array.reduce(
-      (acc, row) => ((acc[row[0]] = [...(acc[row[0]] || []), row[1]]), acc),
-      {}
-    );
+    const arrayToObject = (array) => {
+    // array 에 대해 reduce 메서드를 적용한다.
+      return array.reduce((acc, row) => {
+      // reduce의 초기값은 빈 객체이고, row는 2차원 배열의 한 행을 말한다.
+      // 하나의 행은 [신고한 사람, 신고당한 사람] 을 갖고 있으므로, 구조 분해 할당을 통해 나눈다.
+      const [key, object] = row;
+      // acc는 객체다. key에 value로 배열을 넣는 것인데,
+      // acc[key]에 이미 데이터가 존재하면 spread 연산자로 펼쳐 넣어주고
+      // 만약 없다면 빈 배열의 데이터를 펼쳐 넣어준다. 즉 아무것도 안넣는다.
+      // 그 다음 object 값을 배열에 담아 넣는다.
+      // console.log(acc[key])
+      if(acc[key] && acc[key].includes(object)){
+
+      } else {
+        acc[key] = [...(acc[key] || []), object];
+      }
+      return acc;
+      }, {});
+    };
     rep = arrayToObject(rep);
-    // console.log(rep);
-    dec.forEach((x) => {
+    //console.log(rep);
+    let keys = Object.keys(rep);
+    rep[keys].forEach((x) => {
       count[x] = (count[x] || 0)+1;
       if(count[x] === k){
         answer.push(x);

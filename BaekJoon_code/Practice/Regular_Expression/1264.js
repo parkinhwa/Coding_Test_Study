daca2f4761594cb2f6f375017ecb0db148c1181a
parkinhwa <inhwa6548@naver.com>
2022-02-11 07:52:45 +0000

    ..

---
+++ BaekJoon_code/Practice/Regular_Expression/1264.js
@@ -0,0 +1,21 @@
+// let input = ["How are you today?",
+// "Quite well, thank you, how about yourself?",
+// "I live at number twenty four.",
+// "#"];
+let input = require('fs').readFileSync('/dev/stdin').toString().split("\n");
+
+let gather = /[aeiou]/i;
+
+for(let i = 0; i < input.length; i++){
+  let count = 0;
+  if(input[i] === "#"){
+    break;
+  } else {
+    for(let j = 0; j < input[i].length; j++){
+      if(input[i][j].match(gather)){
+        count++;
+      }
+    }
+    console.log(count);
+  }
+}

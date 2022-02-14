ac36212269d4e712fd72ae38af33b0cb8b011e45
parkinhwa <inhwa6548@naver.com>
2022-02-14 12:54:22 +0000

    ..

---
+++ BaekJoon_code/Practice/Regular_Expression/9342.js
@@ -0,0 +1,7 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split("\n");
+let T = input.shift();
+let abcdef = /[ABCDEF]/;
+let afc = /[AFC]/;
+for(let i = 0; i < T; i++){
+  
+}

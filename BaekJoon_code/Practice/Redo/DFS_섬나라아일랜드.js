6c08d3db37fe8ff0416319cf60c0dd34502a4b2f
parkinhwa <inhwa6548@naver.com>
2022-02-17 08:26:38 +0000

    ..

---
+++ BaekJoon_code/Practice/Redo/DFS_섬나라아일랜드.js
@@ -0,0 +1,36 @@
+function solution(arr){
+  let answer = 0;
+  let n = arr.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+  function DFS(x, y){
+     arr[x][y]=0;
+     for(let k=0; k<8; k++){
+         let nx=x+dx[k];
+         let ny=y+dy[k];
+         if(nx>=0 && nx<n && ny>=0 && ny<n && arr[nx][ny]===1){
+             DFS(nx, ny);
+         }
+     } 
+  }
+  for(let i = 0; i < n; i++){
+    for(let j = 0; j < n; j++){
+      // console.log(i, j)
+      if(arr[i][j] === 1){
+        arr[i][j] = 0;
+        answer++;
+        DFS(i, j);
+      }
+    }
+  }
+  return answer;
+}
+let arr=[[1, 1, 0, 0, 0, 1, 0], 
+         [0, 1, 1, 0, 1, 1, 0],
+         [0, 1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 0, 1, 1],
+         [1, 1, 0, 1, 1, 0, 0],
+         [1, 0, 0, 0, 1, 0, 0],
+         [1, 0, 1, 0, 1, 0, 0]];
+
+console.log(solution(arr));

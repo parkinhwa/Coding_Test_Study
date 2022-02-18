d0b78b3e516c87fd76a3f627440941464b5f69f2
parkinhwa <inhwa6548@naver.com>
2022-02-18 06:07:21 +0000

    ..

--- BaekJoon_code/Practice/Redo/DFS_섬나라아일랜드.js
+++ BaekJoon_code/Practice/Redo/DFS_섬나라아일랜드.js
@@ -16,6 +16,7 @@ function solution(arr){
   for(let i = 0; i < n; i++){
     for(let j = 0; j < n; j++){
       if(arr[i][j] === 1){
+        arr[i][j] = 0;
         answer++;
         DFS(i, j);
       }

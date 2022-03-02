2cfa2f16f010f5f6ea9faf4bf97c67c45a64411f
parkinhwa <inhwa6548@naver.com>
2022-03-02 07:05:39 +0000

    ..

--- BaekJoon_code/Level1/A+B-7.js
+++ BaekJoon_code/Level1/A+B-7.js
@@ -1,5 +1,6 @@
 let input = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n");
 let n = Number(input.shift());
+
 for(let i = 0; i < n; i++){
   let [a, b] = input.shift().split(" ").map(Number);
   let num = i+1;

b6702c5ef6bf4055dc3a356668d82a5143bea032
parkinhwa <inhwa6548@naver.com>
2022-03-02 07:02:25 +0000

    ..

---
+++ BaekJoon_code/Level1/A+B-7.js
@@ -0,0 +1,8 @@
+let input = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n");
+let n = Number(input.shift());
+for(let i = 0; i < n; i++){
+  let [a, b] = input.shift().split(" ").map(Number);
+  let num = i+1;
+  let sum = a+b;
+  console.log("Case #"+num+": "+sum);
+}

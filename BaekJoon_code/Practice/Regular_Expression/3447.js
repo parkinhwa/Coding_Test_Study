d1a0eec4278061b44f12514ed137f9aa89a44a62
parkinhwa <inhwa6548@naver.com>
2022-03-08 09:42:23 +0000

    ..

--- BaekJoon_code/Practice/Regular_Expression/3447.js
+++ BaekJoon_code/Practice/Regular_Expression/3447.js
@@ -1,6 +1,9 @@
 let input = require('fs').readFileSync("/dev/stdin").toString().split("\n");
 let Regex = /BUG/g;
 for(let i = 0; i < input.length-1; i++){
-  input[i] = input[i].replace(Regex, "");
-  console.log(input[i]);
+  let str = input[i]
+  while(Regex.test(str)){
+    str = str.replace(Regex, "");
+  }
+  console.log(str);
 }

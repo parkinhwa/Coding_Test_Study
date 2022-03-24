e5921388a7f6bad91b3ff21407e76fba7d2213ce
parkinhwa <inhwa6548@naver.com>
2022-03-24 09:25:08 +0000

    이진탐색구현

--- Prgrms-Devcourse/이진탐색.js
+++ Prgrms-Devcourse/이진탐색.js
@@ -81,61 +81,59 @@ class BinarySearchTree {
        } else {
          parentNode.left = null;
        }
-       currentNode = null;
       // 하나의 서브트리를 가지는 경우
     } else if(currentNode.left === null && currentNode.right !== null){
       parentNode.right = currentNode.right;
-      currentNode = null;
     } else if(currentNode.left !== null && currentNode.right === null){
-      parentNode.left = currentNode.left;
-      currentNode = null;      
+      parentNode.left = currentNode.left; 
     } else {
       // 두개의 서브트리를 가지는 경우
+      
       // 왼쪽 가장 큰 값과 바꿀 때
-      // let chageNode = currentNode;
-      // while(chageNode.left !== null || chageNode.right !== null){
-      //   // parentNode = chageNode;
+      // let changeNode = currentNode.left; 
+      // // 현재 선택된 노드가 왼쪽 오른쪽 노드 모두 없을때까지 순환
+      // while(changeNode.left !== null || changeNode.right !== null){
       //   //console.log(currentNode.value, currentNode.left, currentNode.right)
-      //   if(chageNode.left === null && chageNode.right !== null){
-      //     chageNode = chageNode.right;
+      //   // 오른쪽으로 가다가 오른쪽이 없고 왼쪽만 있다면 왼쪽으로 튼다.
+      //   if(changeNode.left !== null && changeNode.right === null){
+      //     changeNode = chageNode.left;
       //     //console.log(currentNode)
       //   } else {
       //     //console.log(currentNode.value)
-      //     chageNode = chageNode.left;
+      //     changeNode = changeNode.right;
       //     //console.log(currentNode.value)
       //   }
       // }
-      // console.log(chageNode.value, currentNode.value);
-      // chageNode.right = currentNode.right;
-      // chageNode.left = currentNode.left;
-      // parentNode.left = chageNode;
-      // currentNode = null;
       
       // 오른쪽 가장 작은 값과 바꿀 때     
-      let chageNode = currentNode;
-      while(chageNode.left !== null || chageNode.right !== null){
-        // parentNode = chageNode;
+      let changeNode = currentNode.right;
+      // 현재 선택된 노드가 왼쪽 오른쪽 노드 모두 없을때까지 순환
+      while(changeNode.left !== null || changeNode.right !== null){
         //console.log(currentNode.value, currentNode.left, currentNode.right)
-        if(chageNode.left !== null && chageNode.right === null){
-          chageNode = chageNode.left;
+        // 왼쪽으로 가다가 왼쪽이 없고 오른쪽만 있다면 오른쪽으로 튼다.
+        if(changeNode.left === null && changeNode.right !== null){
+          changeNode = changeNode.right;
           //console.log(currentNode)
         } else {
           //console.log(currentNode.value)
-          chageNode = chageNode.right;
+          changeNode = changeNode.left;
           //console.log(currentNode.value)
         }
       }
-      console.log(chageNode.value, currentNode.value);
-      chageNode.right = currentNode.right;
-      chageNode.left = currentNode.left;
-      if(chageNode.value < parentNode.value){
-        parentNode.left = chageNode;
-      } else {
-        parentNode.right = chageNode;
+
+      // 변경할 노드의 오른쪽 노드에 지울 노드의 오른쪽 노드 붙임
+      changeNode.right = currentNode.right;
+      // 변경할 노드의 왼쪽 노드에 지울 노드의 왼쪽 노드 붙임
+      changeNode.left = currentNode.left;
+      
+      // 변경할 노드값이 부모값보다 작으면 나는야 왼쪽 자식 
+      if(changeNode.value < parentNode.value){
+        parentNode.left = changeNode; // 부모 왼쪽에 붙임
+      } else { // 아니면 오른쪽 자식
+        parentNode.right = changeNode; // 부모 오른쪽에 붙음
       }
-      currentNode = null;
     }
-    
+    currentNode = null; // 지울 노드 = null
   }
   has(value) {
     let currentNode = this.root;

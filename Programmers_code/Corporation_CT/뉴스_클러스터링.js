3340823d4bbdca56603b60e0d181dbfdf1d9505e
parkinhwa <inhwa6548@naver.com>
2022-10-14 11:15:11 +0000

    ..

---
+++ Programmers_code/Corporation_CT/뉴스_클러스터링.js
@@ -0,0 +1,39 @@
+function subset (str) {
+    const reg = /[a-z]/g
+    const str_subset = []
+    for(let i = 0; i < str.length-1; i++) {
+        const str_split = str[i] + str[i+1]
+        const str_match = str_split.match(reg) || []
+        if (str_match.length === 2) str_subset.push(str_split)
+    }
+    return str_subset
+}
+
+function solution(str1, str2) {
+    let answer = 0;
+    const str1_lower = str1.toLowerCase()
+    const str2_lower = str2.toLowerCase()
+    const str1_subset = subset(str1_lower)
+    const str2_subset = subset(str2_lower)
+    const set = new Set([...str1_subset, ...str2_subset]);
+    let union = 0;
+    let intersection = 0;
+
+    set.forEach(item => {
+        const has1 = str1_subset.filter(x => x === item).length;
+        const has2 = str2_subset.filter(x => x === item).length;
+        union += Math.max(has1, has2);
+        intersection += Math.min(has1, has2);
+    })
+    console.log(union,intersection)
+    return union === 0 ? 65536 : Math.floor(intersection / union * 65536);
+    
+//     const intersection = Math.min(str1_subset.filter(s => str2_subset.includes(s)).length, str2_subset.filter(s => str1_subset.includes(s)).length)
+    
+//     const union = str1_subset.length + str2_subset.length - intersection
+    
+//     console.log(intersection, union)
+//     if(union === 0) answer = 65536
+//     else answer = Math.floor(intersection / union * 65536)
+//     return answer;
+}

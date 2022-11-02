22ec5034509da1b0e020a7b34edd850491d865bb
parkinhwa <inhwa6548@naver.com>
2022-11-02 11:28:59 +0000

    구현

--- Programmers_code/Implement/햄버거만들기.js
+++ Programmers_code/Implement/햄버거만들기.js
@@ -1,21 +1,18 @@
 function solution(ingredient) {
     let answer = 0;
-    const hamburger = "1231"
     const arr = []
-    ingredient = ingredient.map((num) => num.toString())
     for(let i = 0; i < ingredient.length; i++) {
         arr.push(ingredient[i])
         if(arr.length >= 4) {
-            const str = arr[arr.length-4] + arr[arr.length-3] + arr[arr.length-2] + arr[arr.length-1]
-            if(str === hamburger) {
+            if(arr[arr.length-4] === 1 && 
+               arr[arr.length-3] === 2 && 
+               arr[arr.length-2] === 3 && 
+               arr[arr.length-1] === 1) {
                 answer++
-                arr.pop()
-                arr.pop()
-                arr.pop()
-                arr.pop()
+                arr.splice(-4)
             }
         }
     }
-
+    
     return answer;
 }
